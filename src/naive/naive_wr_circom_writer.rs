de62ef7a5657d47b0ec04b7fd093fd4dd4410f47
ecmargo <ecmargolin@gmail.com>
2023-12-11 14:28:17 -0500

    edited nwr circom

--- src/naive/naive_wr_circom_writer.rs
+++ src/naive/naive_wr_circom_writer.rs
@@ -210,36 +210,17 @@ pub fn make_main(doc_len: usize,deltas:usize,n_accepting:usize, n_char: usize, n
         signal input next_state; 
         signal input char;
 
-        signal input step_in[4];
-        signal output step_out[4];
+        signal input step_in[3];
+        signal output step_out[3];
 
         signal running_hash <== step_in[1];
         signal index <== step_in[0];
-        signal cur_state_hash_in <== step_in[3];
-
-        component indexIsZero = IsZero();
-        indexIsZero.in <== index;
-
-        component cur_state_hash = Poseidon(1);
-        cur_state_hash.inputs[0] <== cur_state;
-
-        component switcher  = Switcher();
-        switcher.sel <== indexIsZero.out; 
-        switcher.L <== cur_state_hash.out; 
-        switcher.R <== cur_state_hash_in;
-
-        cur_state_hash_in === switcher.outL;
-
-        var temp_hash;
-        temp_hash = cur_state_hash.out;
 
         component valid_state;
         
         valid_state = IsValidTrans();
         valid_state.curIndex <== cur_state*{n_states}*{n_char} + char*{n_states} + next_state;
         valid_state.out === 0;
-
-        cur_state*indexIsZero.out === 0;
 
         component valid_match;
         valid_match = IsValidMatch();
@@ -250,14 +231,9 @@ pub fn make_main(doc_len: usize,deltas:usize,n_accepting:usize, n_char: usize, n
 
         hash.inputs[1] <== char;
 
-        component next_state_hash = Poseidon(1);
-        next_state_hash.inputs[0] <== next_state;
-
         step_out[0] <== index + 1;
         step_out[1] <== hash.out;
         step_out[2] <== valid_match.out;
-        step_out[3] <== next_state_hash.out;
-
     }}
     
     component main {{ public [step_in] }}= Main();")
